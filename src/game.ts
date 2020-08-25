4ddafc622b5a75d3e515bce9eb0568af41ed8972
dzsunyec <dzsunyec@gmail.com>
2020-08-25 23:52:10 +0200

    workshop version 1.0

--- src/game.ts
+++ src/game.ts
@@ -5,8 +5,8 @@ const player = Camera.instance
 let followPlayer = true
 let targetLocation = new Vector3(8,0,8)
 
-//What distance from the player should the axies stop
-const followDistance = 0
+//What distance around the target axies are scattered
+const scatterRadius = 6
 
 //axies' maximum size in meters!! (for checking out of bounds around estate)
 const axieSize = 1.5
@@ -22,18 +22,24 @@ const boundarySizeZMin = 0 + axieSize
 const boundarySizeZMax = parcelsCountZ * 16 - axieSize
 
 
-//default values for every follower object
+// COMPONENT storing values unique to each axie
+// defaultHeight: height above ground level where axies move around
+// speed
+// randomOffsetX: random position offset around the target, x-axis
+// randomOffsetZ: random position offset around the target, z-axis
+// moving: tells whether the axie is in movement
+// elapsedTime: counting time for bounce animations
 @Component("FollowsPlayer")
 export class FollowsPlayer {  
-  defaultHeight:number = 0
+  defaultHeight:number = 0 
   speed:number = 0.2
-  randomOffsetX:number =  (Math.random()*2-1) * followDistance 
-  randomOffsetZ:number =  (Math.random()*2-1) * followDistance 
+  randomOffsetX:number =  (Math.random()*2-1)
+  randomOffsetZ:number =  (Math.random()*2-1) 
   moving:boolean = true
-  elapsedTime:number = 0
-  followDistance:number = 3
+  elapsedTime:number = 0  
 }
 
+//distance between two points (squared result)
 function distance(pos1: Vector3, pos2: Vector3): number {
   const a = pos1.x - pos2.x
   const b = pos1.z - pos2.z
@@ -42,60 +48,41 @@ function distance(pos1: Vector3, pos2: Vector3): number {
 
 /////////////////////////////////////////////////////////
 // This function below creates an axie with parameters: 
-// modelShape: (required parameter) this should be the .glb or .gltf file of the axie
-// _floatHeight: (optional, default is 2 meters) this will be the height measured from the ground where the axie will move around 
-// _speed: (optional, default is 2 meters/second) the speed if the axie 
-// _flipForward: (optional) make the axie mesh face the other direction  
+// modelShape:  this should be the .glb or .gltf file of the axie
+// _defaultHeight:  this will be the height measured from the ground where the axie will move around 
+// _speed: (meters/second) the speed if the axie 
 /////////////////////////////////////////////////////////
-function spawnAxie(modelShape:GLTFShape, _startPosition?:Vector3, _defaultHeight?:number, _speed?:number, _flipForward?:boolean ){
-    
-  let axieMeshRotate = new Entity()
-  axieMeshRotate.addComponent(modelShape)  
-  axieMeshRotate.addComponent(new Transform({position: new Vector3(0, 0, 0)}))
-
+function spawnAxie(modelShape:GLTFShape, _startPosition:Vector3, _defaultHeight:number, _speed:number){
+ 
   let axie = new Entity()
   axie.addComponent(new Transform({position: new Vector3(8, 2, 8), scale: new Vector3(0.4,0.4,0.4)}))
   axie.addComponent(new FollowsPlayer())
+  axie.addComponent(modelShape)  
 
-  axieMeshRotate.setParent(axie)
-
-  if(_startPosition){
-
-    //Out of bounds spawn
-    if( _startPosition.x > boundarySizeXMax ){
-      _startPosition.x = boundarySizeXMax
-    }
-    if(_startPosition.x < boundarySizeXMin){
-      _startPosition.x = boundarySizeXMin
-    }
-    if( _startPosition.z > boundarySizeZMax ){
-      _startPosition.z = boundarySizeZMax
-    }
-      if(_startPosition.z < boundarySizeZMin){
-      _startPosition.z = boundarySizeZMin
-    }
-    axie.getComponent(Transform).position = _startPosition
+  //is start position within scene boundaries? if no then restrict it to be inside
+  if( _startPosition.x > boundarySizeXMax ){
+    _startPosition.x = boundarySizeXMax
+  }
+  if(_startPosition.x < boundarySizeXMin){
+    _startPosition.x = boundarySizeXMin
+  }
+  if( _startPosition.z > boundarySizeZMax ){
+    _startPosition.z = boundarySizeZMax
+  }
+    if(_startPosition.z < boundarySizeZMin){
+    _startPosition.z = boundarySizeZMin
   }
 
-  if(_defaultHeight){
-    axie.getComponent(FollowsPlayer).defaultHeight = _defaultHeight
-  }
-
-  if(_speed){
-    axie.getComponent(FollowsPlayer).speed = _speed
-  }
-
-  if(_flipForward){    
-    axieMeshRotate.getComponent(Transform).rotation = Quaternion.Euler(0,180,0)
-  }
-
-  axie.getComponent(FollowsPlayer).elapsedTime = Math.random() * 0.5
-  axie.getComponent(FollowsPlayer).followDistance = Math.random() * 6 + followDistance
-  axie.getComponent(FollowsPlayer).randomOffsetX =  (Math.random()*2-1) * axie.getComponent(FollowsPlayer).followDistance
-  axie.getComponent(FollowsPlayer).randomOffsetZ = (Math.random()*2-1) * axie.getComponent(FollowsPlayer).followDistance
+  axie.getComponent(Transform).position = _startPosition  
+  
+  axie.getComponent(FollowsPlayer).defaultHeight = _defaultHeight  
+  axie.getComponent(FollowsPlayer).speed = _speed  
+  axie.getComponent(FollowsPlayer).elapsedTime = Math.random() * 0.5  
+  axie.getComponent(FollowsPlayer).randomOffsetX =  (Math.random()*2-1) * scatterRadius
+  axie.getComponent(FollowsPlayer).randomOffsetZ = (Math.random()*2-1) * scatterRadius
 
   //axie.addComponent(new Billboard())
-
+  
 
   engine.addEntity(axie)  
 }
@@ -119,22 +106,19 @@ class PlayerFollowSystem {
 
       let origin = new Vector3(transform.position.x, transform.position.y, transform.position.z)
       let targetPosition = new Vector3(targetLocation.x + objectInfo.randomOffsetX, objectInfo.defaultHeight, targetLocation.z + objectInfo.randomOffsetZ )
+      let lookatPosition = new Vector3(targetLocation.x , 1.0, targetLocation.z )
+           
 
-      let lookatPosition = new Vector3(targetLocation.x , objectInfo.defaultHeight, targetLocation.z )
-     
-      let fraction = dt
-
-      if(fraction > 1){
-        fraction = 1
-      }
-
-      //don't let axie closer than its stored followDistance
+      //move axies towards target, but stop within a certain distance from their targets
       if(distance(targetPosition, transform.position) > 0.25 ){
 
-        objectInfo.moving = true
-        let nextPosition = Vector3.Lerp(origin, targetPosition, fraction)
-        
-        //Out of bounds movement?
+        objectInfo.moving = true  
+       
+        //try to move from origin to target with the speed amount stored in each axie's followPlayer component
+        this.moveVector = targetPosition.subtract(origin).normalize().multiplyByFloats(objectInfo.speed,objectInfo.speed,objectInfo.speed)
+        let nextPosition = origin.add(this.moveVector)
+
+        //Out of bounds movement? if yes, then restrict coordinates to boundaries
         if( nextPosition.x > boundarySizeXMax || nextPosition.x < boundarySizeXMin){
             nextPosition.x = origin.x
         }
@@ -142,17 +126,19 @@ class PlayerFollowSystem {
           nextPosition.z = origin.z
         }
 
-        this.moveVector =  nextPosition.subtract(origin).normalize().multiplyByFloats(objectInfo.speed,objectInfo.speed,objectInfo.speed)         
-
-        transform.position.addInPlace(this.moveVector)
+        //move the axie to new position
+        transform.position.copyFrom(nextPosition)
       }
       else{
         objectInfo.moving = false
       }
 
-      //gradually rotate towards the target point (player position)
-      let targetRotation = Quaternion.FromToRotation(Vector3.Forward(), lookatPosition.subtract(origin.multiplyByFloats(1,0,1)))        
-       transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, dt*4)  
+      // get the exact rotation that turns a Forward looking axie towards the target point 
+      //let targetRotation = Quaternion.FromToRotation(Vector3.Forward(), lookatPosition.subtract(origin.multiplyByFloats(1,0,1)))    
+      // gradually rotate axies towards the above target orientation     
+      //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, dt*4)  
+
+       transform.lookAt(lookatPosition)
     } 
   }
 }
@@ -165,7 +151,8 @@ class bounceSystem {
 
   //jump height
   amplitude = 0.4
-  group = engine.getComponentGroup(FollowsPlayer)
+  frequency = 5
+  group = engine.getComponentGroup(FollowsPlayer,Transform)
 
   update(dt: number) {   
 
@@ -176,13 +163,17 @@ class bounceSystem {
 
       objectInfo.elapsedTime += dt
 
-      //bounce only while moving
+      //bounce higher and faster while moving
       if(objectInfo.moving){
-        transform.position.y = objectInfo.defaultHeight + Math.abs(Math.sin(objectInfo.elapsedTime*10) * this.amplitude) 
+        this.amplitude = 0.4
+        this.frequency = 12         
       }
       else{
-        transform.position.y = objectInfo.defaultHeight
-      }     
+        this.amplitude = 0.05
+        this.frequency = 5
+      }   
+      //bounce movement (bouncy sine wave)
+      transform.position.y = objectInfo.defaultHeight + Math.abs(Math.sin(objectInfo.elapsedTime * this.frequency) * this.amplitude)  
     } 
   }
 }
@@ -205,6 +196,7 @@ let axieShape8 =  new GLTFShape('models/Axie_8.glb')
 let axieShape9 =  new GLTFShape('models/Axie_9.glb')
 let axieShape10 =  new GLTFShape('models/Axie_10.glb')
 
+
 let axieShapeArray = []
 
 axieShapeArray.push(axieShape1)
@@ -218,13 +210,29 @@ axieShapeArray.push(axieShape8)
 axieShapeArray.push(axieShape9)
 axieShapeArray.push(axieShape10)
 
-//Spawn axies: (glb/gltf shape's name, start position vector, height above ground, movement speed, flip forward direction of object)
+
 
 for(let i=0; i<50; i++){
-  spawnAxie(axieShapeArray[i%10], new Vector3(Math.random()*parcelsCountX*16,2,Math.random()*parcelsCountX*16), 0, Math.random()*0.15 + 0.1 , false)
+  //get random coords on x and z axes
+  let randomSpawnX = Math.random()*parcelsCountX*16
+  let randomSpawnZ = Math.random()*parcelsCountZ*16
+
+  //Spawn axies: (
+  //glb shape's name, 
+  //start position vector, 
+  //height above ground, 
+  //movement speed 
+  //) 
+
+  spawnAxie(
+    axieShapeArray[i%10], 
+    new Vector3(randomSpawnX, 0.1, randomSpawnZ), 
+    0.1, 
+    Math.random() * 0.15 + 0.1
+  )
 }
 
-//Create target marker
+//Create target marker, but hide it underground
 let markerShape =  new GLTFShape('models/marker.glb')
 
 let marker = new Entity()
@@ -234,7 +242,7 @@ marker.addComponent(new Billboard(false,true,false))
 engine.addEntity(marker)
 
 
-//Ground
+//Ground grass tiles
 let groundShape = new GLTFShape('models/ground.glb')
 
 for(let i = 0; i < parcelsCountX; i++){
@@ -245,21 +253,23 @@ for(let i = 0; i < parcelsCountX; i++){
     engine.addEntity(ground)
   }
 }
+// ground collider box for clicking
+let groundCollider = new Entity()
+groundCollider.addComponent(new BoxShape())
+groundCollider.addComponent(new Transform({
+  position:new Vector3(parcelsCountX*16/2,0,parcelsCountZ*16/2),
+  scale: new Vector3(parcelsCountX * 16 - 4, 0.1, parcelsCountZ * 16 - 4)}))
+engine.addEntity(groundCollider)
 
-let ground = new Entity()
-ground.addComponent(new BoxShape())
-ground.addComponent(new Transform({position:new Vector3(parcelsCountX*16/2,0,parcelsCountZ*16/2),
-scale: new Vector3(parcelsCountX*16-4,0.1,parcelsCountZ*16-4)}))
-engine.addEntity(ground)
 
-
-
+//axies follow the player and we hide the marker underground
 function makeFollowPlayer(){
 
   followPlayer = true
   marker.getComponent(Transform).position.set(8,-20,8)
 }
 
+//axies follow the target marker, which is moved to _loc vector input
 function makeFollowLocation(_loc:Vector3){
   followPlayer = false
   targetLocation = _loc
@@ -267,6 +277,7 @@ function makeFollowLocation(_loc:Vector3){
 
 }
 
+//INPUTS
 const input = Input.instance
 
 //click to send axies to a location
